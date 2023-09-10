aa8914aad4ecaa378e6a100fc766defdd5a8ab81
ПК <china.a7en@gmail.com>
2023-09-10 17:02:45 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "chyngyz";
+        student.lastName = "Esenbaev";
+        student.age = 16;
+        System.out.println(student.name+"\n"+student.lastName+"\n"+student.age);
+    }
+}
